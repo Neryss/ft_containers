a9ca23d38f6655f3399bb73d26faad2aa933bb7d
neryss <neryssistos@gmail.com>
2021-10-20 00:45:38 +0200

    basic template

--- lib/Vector.hpp
+++ lib/Vector.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+namespace ft {
+	template <typename T>
+	class Vector {
+		private:
+			/* data */
+		public:
+			Vector();
+			~Vector();
+	};
+	template <typename T>
+	Vector<T>::Vector() {}
+
+	template <typename T>
+	Vector<T>::~Vector() {}
+}
