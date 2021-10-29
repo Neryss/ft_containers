8f936b50783f823eee4b9ac33d9933ddede50eab
neryss <neryssistos@gmail.com>
2021-10-29 16:16:41 +0200

    traits should be done

--- lib/iterators/iterator_traits.hpp
+++ lib/iterators/iterator_traits.hpp
@@ -17,6 +17,19 @@ namespace ft {
 	struct iterator_traits<T*>
 	{
 		typedef ptrdiff_t difference_type;
-		typedef 
-	}
+		typedef T value_type;
+		typedef T *pointer;
+		typedef T &reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	template <class T>
+	struct iterator_traits<const T*>
+	{
+		typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef const T *pointer;
+		typedef const T &reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
 }
